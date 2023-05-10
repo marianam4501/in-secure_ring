db812970772be834751706ff75f96ed0658f5527
marianam4501 <mariana@primarix.com>
2023-05-10 00:46:13 -0600

    Add File Manager class to read and write files

---
+++ src/file_manager.hpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class FileManager {
+ public:
+  static std::string Read(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file) {
+      std::cerr << "Error: Could not open file '" << filename << "' for reading." << std::endl;
+      return "";
+    }
+    std::string content((std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+    file.close();
+    return content;
+  }
+
+  static bool Write(const std::string& content, const std::string& filename) {
+    std::ofstream file(filename);
+    if (!file) {
+      std::cerr << "Error: Could not open file '" << filename << "' for writing." << std::endl;
+      return false;
+    }
+    file << content;
+    file.close();
+    return true;
+  }
+};

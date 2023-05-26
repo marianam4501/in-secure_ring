521eefc4f222660063f48d7e424fe7da94b0a710
marianam4501 <mariana@primarix.com>
2023-05-26 01:41:36 -0600

    Create MessageGenerator class and test main

---
+++ src/messageGeneratorTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MessageGenerator.hpp"
+
+int main(){
+    std::string message = "";
+    std::cout << "Enter your message: ";
+    std::getline(std::cin, message);
+    std::cout << std::endl;
+    MessageGenerator::createMessage(message);
+    return 0;
+}

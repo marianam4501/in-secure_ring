bb0336a3447c586eb82e43537632df0ff0a8e31f
marianam4501 <mariana@primarix.com>
2023-06-05 22:37:36 -0600

    Refactor MessageGenerator

--- src/EAEA/MessageGenerator.cpp
+++ src/EAEA/MessageGenerator.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "FileManager.hpp"
-#include "Cryptographer.hpp"
 #include <cstdlib>
 std::string getCommandOutput(const std::string& command);
 int main(){
@@ -13,45 +12,39 @@ int main(){
     std::getline(std::cin, message);
     std::cout << std::endl;
     FileManager fileManager;
-    Cryptographer cryptographer;
-    std::string certPath = "";
-    std::cout << "Enter the path of your certificate: ";
-    std::getline(std::cin, certPath);
-    std::cout << std::endl;
+    std::string certPath = "/home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/certs/"+username+".crt";
     std::string keyPath = "";
     std::cout << "Enter the path of your key: ";
     std::getline(std::cin, keyPath);
     std::cout << std::endl;
-    std::string successMessage = "Verified OK";
-    std::string failureMessage = "Verification Failure";
     //sign message
     std::string signature;
     std::string command = "echo -n " + message + " | openssl dgst -sha256 -sign " + keyPath + " -out /home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/private/firma.sha256";
+    //std::cout << command <<std::endl;
     std::string fullCommand = "bash -c \"" + command + "\"";
     std::system(fullCommand.c_str());
 
     signature = fileManager.Read("/home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/private/firma.sha256");
 
-    std::cout << signature << std::endl;
+    //std::cout << signature << std::endl;
     if (!signature.empty()) {
         std::string fileContent = username + "\n" + signature + "\n" + message;
         fileManager.saveMessage(fileContent, "/home/mariana.murilloquintana/EAEA/"+username);
-        std::cout << "Success: The message was sign: "<< signature << std::endl;
+        //std::cout << "Success: The message was sign: "<< signature << std::endl;
     } else {
         std::cout << "Error: Not able to sign the message. \n";
         return 1;
     }
 
-    //fileManager.ReadMessageFile("/home/mariana.murilloquintana/EAEA/mariana.murilloquintana/000034.txt");
-
-    std::string verifyCommand = "echo -n "+message+" | openssl dgst -sha256 -sign "+keyPath+" -out firma.sha256";
-    std::cout << "Verifying result: " << getCommandOutput(verifyCommand) << std::endl;
+    // std::string extractPubKey = "openssl x509 -pubkey -noout -in "+certPath+" > /home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/private/pubkey.pem";
+    // std::system(extractPubKey.c_str());
+    // std::string verifyCommand = "bash -c \"echo -n '" + message + "' | openssl dgst -sha256 -verify <(openssl rsa -pubin -inform PEM -in /home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/private/pubkey.pem) -signature /home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/private/firma.sha256\"";
+    // std::cout << "\n\n" <<verifyCommand <<std::endl;
+    // std::cout << "Verifying result: " << getCommandOutput(verifyCommand) << std::endl;
 
     return 0;
-}//echo -n "STRING_A_FIRMAR" | openssl dgst -sha256 -sign privatekey.pem -out firma.sha256
-//echo -n "STRING_A_FIRMAR" | openssl dgst -sha256 -sign /home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/mariana_key.pem -out firma.sha256
-//echo -n "STRING_A_FIRMAR" | openssl dgst -sha256 -sign privatekey.pem -out firma.sha256
-//echo -n "STRING_A_FIRMAR" | openssl dgst -sha256 -verify <(openssl x509 -pubkey -noout -in /home/mariana.murilloquintana/in-secure_ring/src/EAEA/ca/certs/mariana.crt) -signature firma.sha256
+}
+
 std::string getCommandOutput(const std::string& command) {
     std::string result;
     char buffer[128];
@@ -67,14 +60,3 @@ std::string getCommandOutput(const std::string& command) {
     pclose(pipe);
     return result;
 }
-
-/*
-
-int main() {
-    std::string command = "ls -l"; // Ejemplo: ejecutar el comando 'ls -l'
-    std::string output = getCommandOutput(command);
-    std::cout << "Salida del comando: " << output << std::endl;
-    
-    return 0;
-}
-*/

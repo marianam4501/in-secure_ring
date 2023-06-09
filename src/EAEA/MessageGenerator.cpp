31ecfe627b9d723cc811ac8fadf650794f0da11d
marianam4501 <mariana@primarix.com>
2023-06-09 14:43:11 -0600

    Validar usuarios

--- src/EAEA/MessageGenerator.cpp
+++ src/EAEA/MessageGenerator.cpp
@@ -1,48 +1,57 @@
 #include <iostream>
 #include "FileManager.hpp"
 #include <cstdlib>
+#include <algorithm>
+
+const std::string PATH_USER = "mariana.murilloquintana";
 std::string getCommandOutput(const std::string& command);
+bool validUsername(std::string username);std::vector<std::string> usernames = {"manuel.arroyo", "rodrigo.piedra", "fabian.gonzalezrojas", 
+        "cesar.lopezurena", "nayeri.azofeifa", "leonel.campos", "angie.castillocampos", "mariana.murilloquintana", 
+        "jeremy.vargasartavia", "valery.murcia"};
 int main(){
     std::string username = "";
     std::cout << "Enter your username: ";
     std::getline(std::cin, username);
     std::cout << std::endl;
-    std::string message = "";
-    std::cout << "Enter your message: ";
-    std::getline(std::cin, message);
-    std::cout << std::endl;
-    FileManager fileManager;
-    std::string certPath = "/home/fabian.gonzalezrojas/in-secure_ring/src/EAEA/ca/certs/"+username+".crt";
-    std::string keyPath = "";
-    std::cout << "Enter the path of your key: ";
-    std::getline(std::cin, keyPath);
-    std::cout << std::endl;
-    //sign message
-    std::string signature;
-    std::string command = "echo -n " + message + " | openssl dgst -sha256 -sign " + keyPath + " -out /home/fabian.gonzalezrojas/in-secure_ring/src/EAEA/ca/private/firma.sha256";
-    //std::cout << command <<std::endl;
-    std::string fullCommand = "bash -c \"" + command + "\"";
-    std::system(fullCommand.c_str());
+    if(validUsername(username)){
+        std::string message = "";
+        std::cout << "Enter your message: ";
+        std::getline(std::cin, message);
+        std::cout << std::endl;
+        FileManager fileManager;
+        std::string certPath = "/home/"+PATH_USER+"/in-secure_ring/src/EAEA/ca/certs/"+username+".crt";
+        std::string keyPath = "";
+        std::cout << "Enter the path of your key: ";
+        std::getline(std::cin, keyPath);
+        std::cout << std::endl;
+        //sign message
+        std::string signature;
+        const std::string command = "echo -n " + message + " | openssl dgst -sha256 -sign " + keyPath + " -out /home/"+PATH_USER+"/in-secure_ring/src/EAEA/ca/private/firma.sha256";
+        //std::cout << command <<std::endl;
+        const std::string fullCommand = "bash -c \"" + command + "\"";
+        std::system(fullCommand.c_str());
 
-    signature = fileManager.Read("/home/fabian.gonzalezrojas/in-secure_ring/src/EAEA/ca/private/firma.sha256");
+        signature = fileManager.Read("/home/"+PATH_USER+"/in-secure_ring/src/EAEA/ca/private/firma.sha256");
 
-    //std::cout << signature << std::endl;
-    if (!signature.empty()) {
-        std::string fileContent = username + "\n" + signature + "\n" + message;
-        fileManager.saveMessage(fileContent);
-        //std::cout << "Success: The message was sign: "<< signature << std::endl;
+        //std::cout << signature << std::endl;
+        if (!signature.empty()) {
+            std::string fileContent = username + "\n" + signature + "\n" + message;
+            fileManager.saveMessage(fileContent);
+            //std::cout << "Success: The message was sign: "<< signature << std::endl;
+        } else {
+            std::cout << "Error: Not able to sign the message. \n";
+            return 1;
+        }
     } else {
-        std::cout << "Error: Not able to sign the message. \n";
-        return 1;
+        std::cout << "Error: username does not exist..." << std::endl;
     }
 
-    // std::string extractPubKey = "openssl x509 -pubkey -noout -in "+certPath+" > /home/fabian.gonzalezrojas/in-secure_ring/src/EAEA/ca/private/pubkey.pem";
-    // std::system(extractPubKey.c_str());
-    // std::string verifyCommand = "bash -c \"echo -n '" + message + "' | openssl dgst -sha256 -verify <(openssl rsa -pubin -inform PEM -in /home/fabian.gonzalezrojas/in-secure_ring/src/EAEA/ca/private/pubkey.pem) -signature /home/fabian.gonzalezrojas/in-secure_ring/src/EAEA/ca/private/firma.sha256\"";
-    // std::cout << "\n\n" <<verifyCommand <<std::endl;
-    // std::cout << "Verifying result: " << getCommandOutput(verifyCommand) << std::endl;
-
     return 0;
+}
+
+bool validUsername(std::string username){
+    auto userFound = std::find(usernames.begin(), usernames.end(), username);
+    return userFound != usernames.end();
 }
 
 std::string getCommandOutput(const std::string& command) {
